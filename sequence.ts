8dfcb3fb9f02e52c53608bee69246be0caf73dfc
notKamui <teillard.jimmy@outlook.fr>
2024-09-23 09:58:35 +0200

    Revert "ALMOST THERE"
    This reverts commit ed19e650ce57f1f81e0e4b944cdad00ea6de7abd.

--- sequence.ts
+++ sequence.ts
@@ -62,7 +62,7 @@ export type Sequence<T, M = {}> = {
   every(predicate: (item: T) => boolean): boolean;
 };
 
-interface SequenceNode<Head, Out, M> extends Sequence<Out, M> {
+interface SequenceNode<Head, Out> extends WithCustomMethods<Out, {}> {
   [WrapAll](downstream: Sink<Out>): Sink<Head>;
   [Consume](sink: TailSink<Out>): void;
 }
@@ -70,18 +70,16 @@ interface SequenceNode<Head, Out, M> extends Sequence<Out, M> {
 const WrapAll = Symbol("__wrapAll");
 const Consume = Symbol("__consume");
 
-function node<Head, In, Out, M>(
+function node<Head, In, Out, M = {}>(
   source: () => Iterator<Head>,
-  previous: SequenceNode<Head, In, M> | null,
+  previous: SequenceNode<Head, In> | null,
   wrap: (downstream: Sink<Out>) => Sink<In>,
   methods: M,
-  wrapAll?: (downstream: Sink<Out>) => Sink<Head>
-): SequenceNode<Head, Out, M> {
+  wrapAll?: (downstream: Sink<Out>) => Sink<Head>,
+): SequenceNode<Head, Out> {
   let consumed = false;
 
   return {
-    ...methods,  // Spread the custom methods
-
     [WrapAll]: wrapAll ?? (downstream => previous![WrapAll](wrap(downstream))),
 
     [Consume](sink: TailSink<Out>) {
@@ -110,7 +108,6 @@ function node<Head, In, Out, M>(
       }
     },
 
-    // Built-in Gatherers with custom methods
     gather({ initializer, integrator, finisher }) {
       const context = initializer?.();
       return node(
@@ -231,7 +228,7 @@ interface SequenceBuilder<M = {}> {
   withGatherer<MethodName extends string, Args extends any[]>(
     name: MethodName,
     gathererFactory: GathererFactory<Args>
-  ): SequenceBuilder<M & Record<MethodName, (...args: Args) => WithCustomMethods<any, M>>>;
+  ): SequenceBuilder<M & Record<MethodName, (...args: Args) => Sequence<any, M>>>;
 
   build(): <T>(iterable: Iterable<T>) => Sequence<T, M>;
 }
@@ -266,16 +263,4 @@ export function createSequenceOfBuilder(): SequenceBuilder {
   };
 }
 
-// Example of creating a custom gatherer factory
-export function fooGathererFactory(someParam: number) {
-  return gatherer({
-    initializer: () => ({ count: 0 }),
-    integrator: (item: number, push: (item: number) => boolean, context: { count: number }) => {
-      context.count += 1;
-      if (context.count <= someParam) {
-        return push(item);
-      }
-      return false;
-    }
-  });
-}
+export const sequenceOf = createSequenceOfBuilder().build();
